f5df644a0e225f781940be32f35203ddd6c90427
Abid Ali <abdsoftfsd@gmail.com>
2022-03-12 17:07:20 +0500

    Generating 3 plots at the same time

--- sessions/06_plot.py
+++ sessions/06_plot.py
@@ -1,22 +1,24 @@
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 sns.set_theme(style="ticks", color_codes=True)
 
-titanic = sns.load_dataset("titanic")
+kashti = sns.load_dataset("titanic")
 
 # These charts are only created by just copy/pasting the code snippets - Don't know what it is.
 
 # BarPlot
-# sns.catplot(x="sex", y="survived", hue="class", kind="bar", data=titanic)
-# plt.show()
+sns.catplot(x="sex", y="survived", hue="class", kind="bar", data=kashti)
+plt.show()
 
 # Count Plots
-# p1 = sns.countplot(x='sex', data=titanic, hue='class')
-# p1.set_title("Plot for counting")
-# plt.show()
+p = sns.countplot(x='sex', data=kashti, hue='class')
+p.set_title("Plot for counting")
+plt.show()
 
 # ScatterPlot
-g = sns.FacetGrid(titanic, row="sex", hue='alone')
+g = sns.FacetGrid(kashti, row="sex", hue='alone')
 g = (g.map(plt.scatter, "age", "fare").add_legend())
 plt.show()
+
+# print(titanic)
