35e71882c1f781193740a90a6ec891437b8b6513
Abid Ali <abdsoftfsd@gmail.com>
2022-03-18 07:51:11 +0500

    Video 29 - Day 12 Pausing chilla to first attending the core python language course to better understand things happening in the code

--- sessions/29/29_assignment_rural_urban_data.py
+++ sessions/29/29_assignment_rural_urban_data.py
@@ -13,11 +13,21 @@ required_data = data[["Element", "Year", "Value"]]
 # Convert into Million
 required_data["Value"] = required_data["Value"].apply(lambda x: int(x // 1000))
 
+# Task: Add a new data row for each year by adding rural and urban value
+# Pseudo code:
+# Read Value for year, sum the values of Value and add a new row
+
 # Task: Getting values based on group
-for group_data in required_data.groupby(["Year"]):
-    print(group_data)
+# for group_data in required_data.groupby(["Year"]):
+#     print(group_data)
     # print("Total population in", x["Year"], "is: ", x)
     # print(group_data, "\n----", group_data[0], group_data[1].iloc[1], "\n=======")
+
+# for index, row in required_data.groupby(["Year"]):
+#     print(index, row)
+#     print(row["Year"], row["Value"])
+
+sum_values = required_data.groupby(["Year"])["Value"].sum()
 
 sns.lineplot(x="Year", y="Value", hue="Element", data=required_data)
 plt.title("Pakistan Rural and Urban Population 1990-2018")
