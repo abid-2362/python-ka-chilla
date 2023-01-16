47a137926aeaa6125c7ad793b7462abbb176c97a
Abid Ali <abdsoftfsd@gmail.com>
2023-01-16 14:16:16 +0500

    Improve BMI assignment

--- sessions/06/bmi_assignment.py
+++ sessions/06/bmi_assignment.py
@@ -3,9 +3,15 @@
 
 # BMI formula = (weight/height)^2
 name = str(input("Enter your name: "))
-# weight = float(input("Helloy " + name + ", please enter your weight in Kg: "))
 weight = float(input(f'Hello {name}, please enter your weight in Kg: '))
 height = float(input(f'Good Job {name}, please enter your height in meters: '))
 
-BMI = (weight / height**2)
-print(f'Thank You {name},\n your BMI is: {BMI}')
+BMI = (weight / height ** 2)
+print(f'''Thank You {name},
+Your BMI is: {BMI}''')
+
+print(f'''BMI Categories:
+Underweight = <18.5
+Normal weight = 18.5–24.9
+Overweight = 25–29.9
+Obesity = BMI of 30 or greater ''')
